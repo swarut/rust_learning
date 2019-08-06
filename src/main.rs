b7322fd65eb6ad24df16a7a0c209fdb57ff107a1
Warut Surapat <wsurapat@gilt.jp>
2019-08-06 11:24:52 +0900

    Add first version of guessing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please guess the number");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Fail to read line");
+
+    println!("You guess : {}", guess);
 }
