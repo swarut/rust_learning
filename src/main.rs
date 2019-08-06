fb8bee9c9d964d5242dbe647b33b88afb96340e6
Warut Surapat <wsurapat@gilt.jp>
2019-08-06 14:47:03 +0900

    Enable loop.

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,25 @@ use rand::Rng;
 
 fn main() {
     let secret = rand::thread_rng().gen_range(1, 101);
+    // let mut guess = String::new();  This wont work, the reason is about the lifetime.
     println! ("The secret number is {}", secret);
-    println!("Please guess the number");
-    let mut guess = String::new();
+    loop {
 
-    io::stdin().read_line(&mut guess)
-        .expect("Fail to read line");
+        println!("Please guess the number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Fail to read line");
+        println!("You guess : {}", guess);
 
-    println!("You guess : {}", guess);
-
-    let guess: i32 = guess.trim().parse().expect("Please input a number");
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: i32 = guess.trim().parse().expect("Please input a number");
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
 }
