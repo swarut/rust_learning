2de3a2b8c7e0ead0bd74f747ac7f3e6f2e4cfa50
Warut Surapat <wsurapat@gilt.jp>
2019-08-06 11:46:17 +0900

    Add random number generation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println! ("The secret number is {}", secret);
     println!("Please guess the number");
     let mut guess = String::new();
 
