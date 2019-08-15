6f8f1b5b3d9c77ac3773fdb35e2fd9505265c681
Warut Surapat <wsurapat@gilt.jp>
2019-08-15 15:09:42 +0900

    Add more codes.

---
+++ file/src/main.rs
@@ -0,0 +1,39 @@
+// use std::fs;
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+
+
+fn main() -> std::io::Result<()> {
+    println!("Please specify path");
+    let mut path = String::new();
+    io::stdin().read_line(&mut path);
+    let path = path.trim();
+    for entry in fs::read_dir(path)? {
+        let dir = entry?;
+        println!("{:?}", dir.path());
+        visit_dirs(&dir.path(), &log);
+    }
+    Ok(())
+
+}
+
+fn log(path: &DirEntry) {
+    println!("Path {:?}", path);
+}
+
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
---
+++ grrs/src/main.rs
@@ -0,0 +1,22 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    // println!("Hello, world! {}", args.pattern);
+}
